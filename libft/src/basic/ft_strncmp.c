b06a359ce0a0d07d7be37957606232b899904b97
Rafaelly <rafaellypoliveira@gmail.com>
2025-12-12 16:17:28 -0300

    refactor: code refactoring

--- libft/src/basic/ft_strncmp.c
+++ libft/src/basic/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rafaoliv <rafaoliv@student.42.fr>          +#+  +:+       +#+        */
+/*   By: devrafaelly <devrafaelly@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 18:06:57 by rafaoliv          #+#    #+#             */
-/*   Updated: 2025/07/24 15:50:32 by rafaoliv         ###   ########.fr       */
+/*   Updated: 2025/12/12 14:54:43 by devrafaelly      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,11 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned char	*str1;
-	unsigned char	*str2;
 	size_t			i;
 
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
 	i = 0;
-	while ((str1[i] || str2[i]) && i < n)
-	{
-		if (str1[i] != str2[i])
-			return (str1[i] - str2[i]);
+	while (((unsigned char)s1[i] && (unsigned char)s2[i]) && i < n
+		&& (unsigned char)s1[i] == (unsigned char)s2[i])
 		i++;
-	}
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
