08235ec1e583647163a261d8d081b0be2b06bb7f
Rafaelly <rafaellypoliveira@gmail.com>
2025-12-16 01:07:15 -0300

    fix: strncmp return

--- libft/src/basic/ft_strncmp.c
+++ libft/src/basic/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: devrafaelly <devrafaelly@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 18:06:57 by rafaoliv          #+#    #+#             */
-/*   Updated: 2025/12/12 14:54:43 by devrafaelly      ###   ########.fr       */
+/*   Updated: 2025/12/16 00:38:30 by devrafaelly      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t			i;
+	size_t	i;
 
 	i = 0;
 	while (((unsigned char)s1[i] && (unsigned char)s2[i]) && i < n
 		&& (unsigned char)s1[i] == (unsigned char)s2[i])
 		i++;
+	if (i == n)
+		return (0);
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
