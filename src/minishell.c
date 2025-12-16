2f39b5b4313e157b3c42e4c92dcbe24c730a8d16
Rafaelly <rafaellypoliveira@gmail.com>
2025-12-16 01:09:48 -0300

    fix: handle whitespace in exit command

--- src/minishell.c
+++ src/minishell.c
@@ -6,49 +6,65 @@
 /*   By: devrafaelly <devrafaelly@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 15:39:45 by gustaoli          #+#    #+#             */
-/*   Updated: 2025/12/13 19:11:19 by devrafaelly      ###   ########.fr       */
+/*   Updated: 2025/12/16 01:02:58 by devrafaelly      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "minishell.h"
+#include "libft.h"
+#include "lexer.h"
 
-static int	input_process(t_token **token, char *input);
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+static int	input_process(char *input);
+static	int	is_exit_cmd(char *input);
 
 int	main(void)
 {
-	t_token	*token;
 	char	*input;
 
-	token = NULL;
 	while (1)
 	{
-		input = readline("42Partners@minishell: ");
-		if (!input_process(&token, input))
+		input = readline(PROMPT);
+		if (!input_process(input))
 			break ;
 	}
 	return (0);
 }
 
-static int	input_process(t_token **token, char *input)
+static int	input_process(char *input)
 {
-	if (!input || ft_strcmp(input, "exit") == 0)
-	{
-		if (input)
-			free(input);
+	t_token	*token;
+	int		i;
+
+	i = 0;
+	if (!input)
 		return (0);
-	}
-	if (*input)
-	{
-		add_history(input);
-		*token = tokenize(*token, input);
-		if (!*token)
-		{
-			free(input);
-			return (1);
-		}
-		free_token(token);
-	}
+	while (ft_isspace(input[i]))
+		i++;
+	if (is_exit_cmd(&input[i]))
+		return (free(input), 0);
+	if (!input[i])
+		return (free(input), 1);
+	add_history(input);
+	token = tokenize(&input[i]);
 	free(input);
+	if (!token)
+		return (1);
+	free_token(&token);
 	return (1);
+}
+
+static	int	is_exit_cmd(char *input)
+{
+	if (ft_strncmp(input, "exit", 4) == 0)
+	{
+		input += 4;
+		while (ft_isspace(*input))
+			input++;
+		return (*input == '\0');
+	}
+	return (0);
 }
