97d300c5a42256b433ded34dd34b7d01cc8df078
Gus1331 <gustavooliver.souza@gmail.com>
2026-01-01 01:07:24 -0300

    feat: adicional check for first env identificator

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,7 +6,11 @@
 /*   By: gustaoli <gustaoli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 22:31:12 by gustaoli          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2025/12/31 23:34:58 by gustaoli         ###   ########.fr       */
+=======
+/*   Updated: 2026/01/01 01:05:26 by gustaoli         ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +31,8 @@ void	ft_unset(char **args, char **envp[], int *status)
 
 static int	validate_arg_name(char *arg)
 {
+	if (!arg || !ft_isalpha(*arg))
+		return (ft_printf("unset: invalid parameter name\n"), -1);
 	while (*arg)
 	{
 		if (!ft_isalnum(*arg))
