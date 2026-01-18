554a430fabc7d0ab719aa9629b06ffdf75e20051
Rafaelly <rafaellypoliveira@gmail.com>
2026-01-18 18:45:24 -0300

    feat: handle no-argument case by printing env

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: devrafaelly <devrafaelly@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 21:46:37 by gustaoli          #+#    #+#             */
-/*   Updated: 2026/01/18 18:23:43 by devrafaelly      ###   ########.fr       */
+/*   Updated: 2026/01/18 18:35:08 by devrafaelly      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "minishell.h"
 #include "error_handling.h"
 
+int			ft_env(t_shell *shell, char **args);
 static int	set_var(t_shell *shell, char *args);
 static int	is_valid_var_name(char *arg);
 static void	cleanup(char *name, char *value);
@@ -24,7 +25,7 @@ int	ft_export(t_shell *shell, char **args)
 
 	shell->status = 0;
 	if (!args[1])
-		return (OK);
+		return (ft_env(shell, args));
 	if (args[1][0] == '-')
 	{
 		shell->status = 1;
